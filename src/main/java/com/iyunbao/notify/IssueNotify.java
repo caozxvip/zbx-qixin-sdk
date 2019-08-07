7026b6303e157a77fe911dbc4373e2a4a08af1eb
caozx <caozxvip@163.com>
2019-08-07 16:26:39 +0800

    i云保通知

---
+++ src/main/java/com/iyunbao/notify/IssueNotify.java
@@ -0,0 +1,237 @@
+package com.iyunbao.notify;
+
+import java.math.BigDecimal;
+
+/**
+ * @Description i云保出单通知
+ * @Author caozhuoxiang
+ * @Date 2019/8/7 15:28
+ **/
+public class IssueNotify {
+
+    private String partner;
+    private String applicantName;
+    private String applicantGender;
+    private String insureTime;
+    private String effectiveTime;
+    private String expiryTime;
+    private BigDecimal price;
+    private String productCode;
+    private String policyNo;
+    private String productName;
+    private String company;
+    private String orderNo;
+    private Integer policyStatus;
+    private String insureData;
+    private String payment;
+    private String expires;
+    private String amount;
+    private String partnerTag;
+    private String insurantName;
+    private String insurantGender;
+    private String insurantBirthday;
+    private String mobile;
+    private String vendorId;
+    private String type;
+    private String productId;
+
+    public String getPartner() {
+        return partner;
+    }
+
+    public void setPartner(String partner) {
+        this.partner = partner;
+    }
+
+    public String getApplicantName() {
+        return applicantName;
+    }
+
+    public void setApplicantName(String applicantName) {
+        this.applicantName = applicantName;
+    }
+
+    public String getApplicantGender() {
+        return applicantGender;
+    }
+
+    public void setApplicantGender(String applicantGender) {
+        this.applicantGender = applicantGender;
+    }
+
+    public String getInsureTime() {
+        return insureTime;
+    }
+
+    public void setInsureTime(String insureTime) {
+        this.insureTime = insureTime;
+    }
+
+    public String getEffectiveTime() {
+        return effectiveTime;
+    }
+
+    public void setEffectiveTime(String effectiveTime) {
+        this.effectiveTime = effectiveTime;
+    }
+
+    public String getExpiryTime() {
+        return expiryTime;
+    }
+
+    public void setExpiryTime(String expiryTime) {
+        this.expiryTime = expiryTime;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getPolicyNo() {
+        return policyNo;
+    }
+
+    public void setPolicyNo(String policyNo) {
+        this.policyNo = policyNo;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public Integer getPolicyStatus() {
+        return policyStatus;
+    }
+
+    public void setPolicyStatus(Integer policyStatus) {
+        this.policyStatus = policyStatus;
+    }
+
+    public String getInsureData() {
+        return insureData;
+    }
+
+    public void setInsureData(String insureData) {
+        this.insureData = insureData;
+    }
+
+    public String getPayment() {
+        return payment;
+    }
+
+    public void setPayment(String payment) {
+        this.payment = payment;
+    }
+
+    public String getExpires() {
+        return expires;
+    }
+
+    public void setExpires(String expires) {
+        this.expires = expires;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getPartnerTag() {
+        return partnerTag;
+    }
+
+    public void setPartnerTag(String partnerTag) {
+        this.partnerTag = partnerTag;
+    }
+
+    public String getInsurantName() {
+        return insurantName;
+    }
+
+    public void setInsurantName(String insurantName) {
+        this.insurantName = insurantName;
+    }
+
+    public String getInsurantGender() {
+        return insurantGender;
+    }
+
+    public void setInsurantGender(String insurantGender) {
+        this.insurantGender = insurantGender;
+    }
+
+    public String getInsurantBirthday() {
+        return insurantBirthday;
+    }
+
+    public void setInsurantBirthday(String insurantBirthday) {
+        this.insurantBirthday = insurantBirthday;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getVendorId() {
+        return vendorId;
+    }
+
+    public void setVendorId(String vendorId) {
+        this.vendorId = vendorId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+}
